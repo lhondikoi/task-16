8f240e4f2f529ada61fd12720170f89e4d234143
lhondikoi <l.hondikoi@gmail.com>
2023-12-25 03:50:11 +0530

    fixed typo

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import '../styles/header.css'
 export default function Header() {
     return (
         <header id="page-header">
-            <h1 id="brand">TechTrailbraze</h1>
+            <h1 id="brand">TechTrailblaze</h1>
             <span id="tagline">Your Path to Success in Code, Security, Data, and Career Journeys</span>
         </header>
     )
